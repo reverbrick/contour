b986bc490804f9bfa36d61e1167410646cd13cdd
Daniel Gorny <reverbrick@interia.pl>
2021-02-01 12:18:01 +0100

    Faust Sawtooth example for arduino

---
+++ Arduino/esp32sawtooth/esp32sawtooth.ino
@@ -0,0 +1,16 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "FaustSawtooth.h"
+
+FaustSawtooth faustSawtooth(48000,8);
+
+void setup() {
+  
+  faustSawtooth.start();
+}
+
+void loop() {
+  faustSawtooth.setParamValue("freq",rand()%(2000-50 + 1) + 50);
+  delay(1000);
+}
